663bfa4be64dad9ad5d456181f17e5efa68887bb
Kendra <kendra@Zipcoders-MacBook-Pro-45.local>
2022-07-02 11:29:06 -0400

    finished project

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,21 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
     public static void main(String[] args ){
+        Scanner askName = new Scanner(System.in);
+        System.out.println("What is your name?");
+        String userName = askName.next();
+        if(userName.toLowerCase().equals("alice")){
+            System.out.println("Hello Alice!");
+        }
+         else if (userName.toLowerCase().equals("bob")){
+            System.out.println("Hello Bob!");
+        } else {
+            System.out.println("");
 
     }
-}
+}}
